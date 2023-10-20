c5180ebce9f31ce59db10913cbef42200c1fbaef
YarneD-1952226 <71635329+YarneD-1952226@users.noreply.github.com>
2023-10-20 11:28:57 +0200

    Added profiling + max k

--- FrequentItemssets.py
+++ FrequentItemssets.py
@@ -1,12 +1,15 @@
 from collections import defaultdict
 from itertools import combinations
 from firstpass import FirstPass
+import cProfile
+
 
 def find_frequent_author_groups(data, threshold):
     # Step 1: Initialize variables
     authors_counts = defaultdict(int)
     frequent_groups = {}  # To store frequent groups and their counts
     k = 1  # Initialize group size -> how many authors published together
+    max_k = 4 # max paired authors in a group to search for
 
     # Step 2: Count occurrences of individual authors
     for publication in data:
@@ -19,7 +22,7 @@ def find_frequent_author_groups(data, threshold):
     while True:
         # Step 3a: Generate candidate itemsets of size k + 1
         if k > 1:
-            candidate_groups = generate_candidate_groups(authors_counts, k)
+            candidate_groups = generate_candidate_groups_k(authors_counts, k)
         if not candidate_groups:
             break
         # Step 3b: Count occurrences of candidate itemsets in the dataset
@@ -27,14 +30,13 @@ def find_frequent_author_groups(data, threshold):
 
         # # # Step 3c: Prune itemsets that do not meet the support threshold
         authors_counts, frequent_groups_k = prune_infrequent_groups(authors_counts, threshold)
-        frequent_groups.update(frequent_groups_k)
+        frequent_groups = frequent_groups_k
         #TODO is authors_counts nodig -> kan je niet gewoon frequent_groups_k terug meegeven?
 
         k += 1
-        if k==4:
+        if k==max_k:
             break
-    # print(len(candidate_groups))
-    # print(frequent_groups_k)
+
 
     return frequent_groups
 
@@ -51,8 +53,6 @@ def generate_candidate_groups_k(authors_counts, k):
         for group2 in frequent_groups[i+1:]:
             new_candidate = frozenset(group1).union(group2)
             if len(new_candidate) == k + 1:
-                if k+1 == 3:
-                    print(new_candidate)
                 candidates.add(new_candidate)
     
     return candidates
@@ -80,12 +80,8 @@ def generate_candidate_groups(authors_counts, k):
     Optimized: Generate candidate groups of size k + 1 by joining frequent groups of size k.
     """
     authors = list(authors_counts)  # Convert to a list for indexing
-
     # Generate combinations of k-sized groups without them being duplicates (e.g., AB = BA)
     candidates = list(combinations(authors, k + 1))
-    if k+1 == 3:
-        print(candidates)
-    # print(candidates)
     return set(candidates)
 
 # def generate_candidate_groups(authors_counts, k):
@@ -133,8 +129,18 @@ def prune_infrequent_groups(authors_counts, threshold):
 # Example usage
 file_path = 'small-xmlparsed.txt'  # Replace with the path to your text file
 fp = FirstPass(file_path)
-fp.process_file()
 
-support_threshold = 4  # threshold of how frequent a author needs to be at min.
+profile = cProfile.Profile()
+profile.enable()
+fp.process_file()
+profile.disable()
+profile.dump_stats("profile_FirstPass.prof")
+
+support_threshold = 2  # threshold of how frequent a author needs to be at min.
+profile.enable()
 frequent_groups = find_frequent_author_groups(fp.data, support_threshold)
+profile.disable()
+profile.dump_stats("profile_SecondPass_k4s2.prof")
 print(frequent_groups)
+
+
